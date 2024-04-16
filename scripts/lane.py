29e3ac5c9ce5d4e25b1d63ffa50e3bfa0b54d0de
jiyooonp <jiyoonp@andrew.cmu.edu>
2024-04-16 17:14:29 -0400

    lane.py complete

--- scripts/lane.py
+++ scripts/lane.py
@@ -7,7 +7,6 @@ from cv_bridge import CvBridge
 import cv2
 import numpy as np
 
-
 class LaneDetectionNode(Node):
     def __init__(self):
         super().__init__('lane_detection_node')
@@ -18,42 +17,49 @@ class LaneDetectionNode(Node):
         self.bridge = CvBridge()
 
     def detect_lanes(self, image_path):
-        # Read the image
         cv_image = cv2.imread(image_path)
 
-        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        yellow_mask = self.segment_yellow(cv_image)
+
+        masked_image = cv2.bitwise_and(cv_image, cv_image, mask=yellow_mask)
+
+        gray = cv2.cvtColor(masked_image, cv2.COLOR_BGR2GRAY)
+
         edges = cv2.Canny(gray, 50, 150)
+
         lines = cv2.HoughLinesP(edges, 1, np.pi/180,
                                 threshold=50, minLineLength=100, maxLineGap=10)
 
-        # Draw detected lines on the original image
         if lines is not None:
-            for line in lines:
-                x1, y1, x2, y2 = line[0]
-                cv2.line(cv_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                for line in lines:
+                    x1, y1, x2, y2 = line[0]
+                    cv2.line(cv_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-        # Convert the OpenCV image to a ROS 2 image message
         lane_detection_msg = self.bridge.cv2_to_imgmsg(
             cv_image, encoding='bgr8')
 
-        # Publish lane detection results
         self.lane_publisher.publish(lane_detection_msg)
 
+    def segment_yellow(self, image):
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+        lower_yellow = np.array([20, 100, 100])
+        upper_yellow = np.array([30, 255, 255])
+
+        yellow_mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
+
+        return yellow_mask
+    
 def main(args=None):
     rclpy.init(args=args)
     lane_detection_node = LaneDetectionNode()
 
-    # Path to the image file
     image_path = '/root/sim_ws/src/lab8_pkg/imgs/lane_sample.png'
 
-    # Perform lane detection on the image
     lane_detection_node.detect_lanes(image_path)
 
-    # Wait for messages to be published
     rclpy.spin_once(lane_detection_node)
 
-    # Clean up
     lane_detection_node.destroy_node()
     rclpy.shutdown()
 
